35f139c22a629f0bbe7604b546a76bbfde1f4bcd
dsuarez72 <cd.suarez10@uniandes.edu.co>
2022-08-17 21:23:39 -0300

    Initial commit

---
+++ R/my_shannon.R
@@ -0,0 +1,17 @@
+
+#' Shannon Diversity
+#'
+#' @param x A vector with values of abundance in the community
+#'
+#' @return  A vector of length 1 with the value of the Shannon diversity
+#' @export
+#'
+#' @examples
+#' com <- c(1, 2, 3)
+#' my_shannon(com)
+my_shannon <- function(x) {
+  x <- x[x > 0]
+  sum_all <- sum(x)
+  pi <- x/sum_all
+  -sum(pi*log(pi))
+}
